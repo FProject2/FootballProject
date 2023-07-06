fec64645fe7ed2ea1e7466d7f63b1ea2bd70666a
SIST <SIST@DESKTOP-2VEJONH>
2023-07-06 17:04:09 +0900

    jiwon

---
+++ FBProject/src/main/java/com/sist/dao/ShopDAO.java
@@ -0,0 +1,12 @@
+package com.sist.dao;
+
+import java.util.*;
+import java.sql.*;
+import com.sist.vo.*;
+import com.sist.common.*;
+
+public class ShopDAO {
+	private Connection conn;
+	private PreparedStatement ps;
+	private CreateDataBase db = new CreateDataBase();
+}
---
+++ FBProject/src/main/java/com/sist/vo/ShopVO.java
@@ -0,0 +1,62 @@
+package com.sist.vo;
+
+public class ShopVO {
+	private int gno, cprice, price, cno;
+	private String goods_name, goods_image, goods_detail, brand, origin;
+	
+	public int getGno() {
+		return gno;
+	}
+	public void setGno(int gno) {
+		this.gno = gno;
+	}
+	public int getCprice() {
+		return cprice;
+	}
+	public void setCprice(int cprice) {
+		this.cprice = cprice;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getCno() {
+		return cno;
+	}
+	public void setCno(int cno) {
+		this.cno = cno;
+	}
+	public String getGoods_name() {
+		return goods_name;
+	}
+	public void setGoods_name(String goods_name) {
+		this.goods_name = goods_name;
+	}
+	public String getGoods_image() {
+		return goods_image;
+	}
+	public void setGoods_image(String goods_image) {
+		this.goods_image = goods_image;
+	}
+	public String getGoods_detail() {
+		return goods_detail;
+	}
+	public void setGoods_detail(String goods_detail) {
+		this.goods_detail = goods_detail;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getOrigin() {
+		return origin;
+	}
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+	
+}
